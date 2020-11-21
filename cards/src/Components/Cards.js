056dd0066c78066b5a4aaadb72cb4ab38e86c3a6
MaysamTurk <71770879+MaysamTurk@users.noreply.github.com>
2020-11-22 00:27:57 +0200

    First react task is done

---
+++ cards/src/Components/Cards.js
@@ -0,0 +1,147 @@
+import './Cards.css'
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaFacebook } from 'react-icons/fa';
+import { FaEdit } from 'react-icons/fa';
+
+
+function Cards() {
+
+
+    return (
+        <div className="All">
+         <div className='Card'>
+            <div className='upper-container'>
+            <FaEdit class="edit"></FaEdit>
+                <div className='image-container'>
+                    <img src="https://avatars2.githubusercontent.com/u/71584331?s=400&u=59e7fe47593366524b5851eeb4a6e4cd4fba1799&v=4" alt='' height="100px" width="100px " />
+                </div>
+            </div>
+            <div className="lower-container">
+                <h3> Jenan</h3>
+                <h4> Managment Information System </h4>
+                <p> Trainee at Coding Academy by Orange on Web and Mobile development, I learned many of programming languaging, such as HTML, CSS, JavaScript, and React.</p>
+                <button><a href="https://github.com/Jenanmusallam"><FaGithub className="icons"></FaGithub></a></button>
+                <button><a href="https://www.linkedin.com/in/jenan-musallam/"><FaLinkedin className="icons"></FaLinkedin></a></button>
+                <button><a href="https://web.facebook.com/search/top/?q=jenan%20a%20musallam&epa=SEARCH_BOX"><FaFacebook className="icons"></FaFacebook></a></button>
+              </div>
+            
+        </div>
+        <div className='Card'>
+            <div className='upper-container'>
+            <FaEdit class="edit"></FaEdit>
+                <div className='image-container'>
+                    <img src="https://avatars3.githubusercontent.com/u/71769659?s=400&u=f79c2bdf655b094836b60d58a15f4a478631e827&v=4" alt='' height="100px" width="100px " />
+                </div>
+            </div>
+            <div className="lower-container">
+                <h3> Taima</h3>
+                <h4> Communication Engineering</h4>
+                <p> Trainee at Coding Academy by Orange on Web and Mobile development, I learned many of programming languaging, such as HTML, CSS, JavaScript, and React.</p>
+                <button><a href="https://github.com/alfokaha-taima"><FaGithub className="icons"></FaGithub></a></button>
+                <button><a href="https://web.facebook.com/taima.alfokaha"><FaLinkedin className="icons"></FaLinkedin></a></button>
+                <button><a href="https://www.linkedin.com/in/taima%E2%80%99-al-fokaha%E2%80%99/"><FaFacebook className="icons"></FaFacebook></a></button>
+                </div>
+        </div><div className='Card'>
+            <div className='upper-container'>
+            <FaEdit class="edit"></FaEdit>
+                <div className='image-container'>
+                    <img src="https://avatars2.githubusercontent.com/u/71770879?s=400&u=2b363f174c196ad0a14ecb5a3a3dbe37550d952b&v=4" alt='' height="100px" width="100px " />
+                </div>
+            </div>
+            <div className="lower-container">
+                <h3> Maysam</h3>
+                <h4> Human Nutition & Dietitics</h4>
+                <p> Trainee at Coding Academy by Orange on Web and Mobile development, I learned many of programming languaging, such as HTML, CSS, JavaScript, and React.</p>
+                <button><a href="https://github.com/MaysamTurk"><FaGithub className="icons"></FaGithub></a></button>
+                <button><a href="https://www.linkedin.com/in/maysamturk/"><FaLinkedin className="icons"></FaLinkedin></a></button>
+                <button><a href="https://web.facebook.com/maysam.turk"><FaFacebook className="icons"></FaFacebook></a></button>
+                </div>
+        </div><div className='Card'>
+            <div className='upper-container'>
+            <FaEdit class="edit"></FaEdit>
+                <div className='image-container'>
+                    <img src="https://avatars3.githubusercontent.com/u/71584804?s=400&u=3b941316bf72ff47d8fef9577006e2b54ba5462e&v=4" alt='' height="100px" width="100px " />
+                </div>
+            </div>
+            <div className="lower-container">
+                <h3> Razan</h3>
+                <h4> Communication Engineering</h4>
+                <p> Trainee at Coding Academy by Orange on Web and Mobile development, I learned many of programming languaging, such as HTML, CSS, JavaScript, and React.</p>
+                <button><a href="https://github.com/Razan-Zuaiter"><FaGithub className="icons"></FaGithub></a></button>
+                <button><a href="https://www.linkedin.com/in/razan-zuaiter/"><FaLinkedin className="icons"></FaLinkedin></a></button>
+                <button><a href="https://web.facebook.com/Razan.zaiter.13"><FaFacebook className="icons"></FaFacebook></a></button>
+                 </div>
+        </div>
+        <div className='Card'>
+            <div className='upper-container'>
+            <FaEdit class="edit"></FaEdit>
+                <div className='image-container'>
+                    <img src="https://avatars2.githubusercontent.com/u/71769566?s=400&u=c066c010c2e4eb481f2d0ef22fd736ca9150ed34&v=4" alt='' height="100px" width="100px " />
+                </div>
+            </div>
+            <div className="lower-container">
+                <h3> Hala</h3>
+                <h4> Communication Engineering</h4>
+                <p> Trainee at Coding Academy by Orange on Web and Mobile development, I learned many of programming languaging, such as HTML, CSS, JavaScript, and React.</p>
+                <button><a href="https://github.com/HalaHyasat"><FaGithub className="icons"></FaGithub></a></button>
+                <button><a href="https://www.linkedin.com/in/hala-alhyasat/"><FaLinkedin className="icons"></FaLinkedin></a></button>
+                <button><a href="https://web.facebook.com/HalaA.AlHyasat"><FaFacebook className="icons"></FaFacebook></a></button>
+                </div>
+        </div>
+        <div className='Card'>
+            <div className='upper-container'>
+            <FaEdit class="edit"></FaEdit>
+                <div className='image-container'>
+                    <img src="https://avatars3.githubusercontent.com/u/60710271?s=460&u=4beabb3ede0403226fde0a59fbc995c15fbaf6c3&v=4" alt='' height="100px" width="100px " />
+                </div>
+            </div>
+            <div className="lower-container">
+                <h3> Dana</h3>
+                <h4>Computer Information System</h4>
+                <p> Trainee at Coding Academy by Orange on Web and Mobile development, I learned many of programming languaging, such as HTML, CSS, JavaScript, and React.</p>
+                <button><a href="https://github.com/danatoughoj"><FaGithub className="icons"></FaGithub></a></button>
+                <button><a href="https://www.linkedin.com/in/dana-toughoj/"><FaLinkedin className="icons"></FaLinkedin></a></button>
+                <button><a href="https://web.facebook.com/toughoj"><FaFacebook className="icons"></FaFacebook></a></button>
+                </div>
+        </div>
+        <div className='Card'>
+            <div className='upper-container'>
+            <FaEdit class="edit"></FaEdit>
+                <div className='image-container'>
+                    <img src="https://avatars3.githubusercontent.com/u/71769638?s=460&u=1a470e089de00fb48d18cefefbc1776e0c808b78&v=4" alt='' height="100px" width="100px " />
+                </div>
+            </div>
+            <div className="lower-container">
+                <h3> Lubna</h3>
+                <h4>  Financial & Banking info. systems</h4>
+                <p> Trainee at Coding Academy by Orange on Web and Mobile development, I learned many of programming languaging, such as HTML, CSS, JavaScript, and React.</p>
+                <button><a href="https://github.com/lubna-almaaweed"><FaGithub className="icons"></FaGithub></a></button>
+                <button><a href="https://www.linkedin.com/in/lubna-almaaweed/"><FaLinkedin className="icons"></FaLinkedin></a></button>
+                <button><a href="https://web.facebook.com/lubna.amjad.5621"><FaFacebook className="icons"></FaFacebook></a></button>
+            </div>
+        </div>
+        <div className='Card'>
+            <div className='upper-container'>
+            <FaEdit class="edit"></FaEdit>
+                <div className='image-container'>
+                    <img src="https://avatars3.githubusercontent.com/u/71769554?s=400&u=9fbfb665d6ef6b1f1af308d43efe467b9e45412b&v=4" alt='' height="100px" width="100px " />
+                </div>
+            </div>
+            <div className="lower-container">
+                <h3> Sarah</h3>
+                <h4> Managment Information System</h4>
+                <p> Trainee at Coding Academy by Orange on Web and Mobile development, I learned many of programming languaging, such as HTML, CSS, JavaScript, and React.</p>
+                <button><a href="https://github.com/Sarah-Eswed"><FaGithub className="icons"></FaGithub></a></button>
+                <button><a href="https://www.linkedin.com/in/sarah-eswed/"><FaLinkedin className="icons"></FaLinkedin></a></button>
+                <button><a href="https://web.facebook.com/sarahaleswed"><FaFacebook className="icons"></FaFacebook></a></button>
+            </div>
+        </div>
+        </div>
+        
+    )
+}
+
+
+
+export default Cards
